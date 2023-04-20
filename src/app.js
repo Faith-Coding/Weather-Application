d4d917e2fc801bad2f47301a2bcafb1941d9033f
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-04-20 08:13:15 +0200

    Added the relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -25,13 +25,14 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   console.log(response.data);
+  console.log(apiUrl);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-
+  let iconElement = document.querySelector("#icon");
 
 
 
@@ -40,15 +41,15 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt = 1000);
-
-
-
-
-
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src",
+  `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "8d174e4c8e7e5c8ec295ac232dd2f775";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+let city = "Berlin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
