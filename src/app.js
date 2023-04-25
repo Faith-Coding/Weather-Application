3dfec5d27e739327a842e167683f5634bc519f66
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-04-25 11:47:54 +0200

    Changed the ApiKey

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ forecastElement.innerHTML = forecastHTML;
 
 }
 function getForecast(coordinates) {
-  let apiKey = "eae061c95483dd066657bfc7525418ed";
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
   console.log(apiUrl);
@@ -116,7 +116,7 @@ getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "eae061c95483dd066657bfc7525418ed";
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
